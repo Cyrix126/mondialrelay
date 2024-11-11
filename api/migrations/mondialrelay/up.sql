cc15e806b1f8f904134726018d9111f69477dec7
Louis-Marie Baer <lm@baermail.fr>
2024-11-11 15:12:03 +0100

    feat: allows multiple shipment for one order

--- api/migrations/mondialrelay/up.sql
+++ api/migrations/mondialrelay/up.sql
@@ -1,6 +1,6 @@
  CREATE TABLE shipments (
    id SERIAL PRIMARY KEY,
-   order_id INT NOT NULL UNIQUE,
+   order_id INT NOT NULL,
    label_url TEXT NOT NULL UNIQUE,
    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
